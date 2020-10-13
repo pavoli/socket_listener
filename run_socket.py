b33003fac04b928b4b846e37185863fcdc4bfb4b
Pavel <pavel.olifer@gmail.com>
2020-10-13 23:30:21 +0300

    Update run_socket.py

--- run_socket.py
+++ run_socket.py
@@ -1,15 +1,8 @@
 # -*- coding: utf-8 -*-
+from threading import Thread
+
 import conf
 import create_socket
-from threading import Thread
-import logging
-
-
-logging.basicConfig(
-    filename=conf.LOG_FILE_NAME,
-    level=logging.DEBUG,
-    format='%(asctime)s | %(message)s'
-)
 
 
 def run(port):
@@ -19,8 +12,7 @@ def run(port):
 
 if __name__ == '__main__':
     for port in conf.PORTS:
-        logging.info('{} | THREAD'.format(port))
+        create_socket.log.debug('{} | THREAD'.format(port))
 
         thread = Thread(target=run, args=(port,))
         thread.start()
-        # thread.join()
