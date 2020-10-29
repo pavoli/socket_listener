8a5ac2c882ffb1d45d726b67c2678a135b568343
Pavel <pavel.olifer@gmail.com>
2020-10-29 19:35:01 +0300

    Update create_socket.py

--- create_socket.py
+++ create_socket.py
@@ -2,22 +2,23 @@
 __author__ = 'p.olifer'
 
 import socket
-from goto import with_goto
-import goto
 
 import work_with_DB
 import conf
 
 
-def get_logger(name=__file__, file='log.txt', encoding='utf-8'):
+def get_logger(name=__file__, file='log/log_{0}.txt', encoding='utf-8'):
     import logging
     log = logging.getLogger(name)
     log.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s | %(message)s')
+    formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
 
     # in file
     from logging import FileHandler
-    fh = FileHandler(filename=file, encoding=encoding)
+    from datetime import datetime
+    
+    now = datetime.now()
+    fh = FileHandler(filename=file.format(now.strftime('%Y%m%d')), encoding=encoding)
     fh.setFormatter(formatter)
     log.addHandler(fh)
 
@@ -40,68 +41,57 @@ class socket_server():
         self.ip = ip
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        log.debug('{} | create socket'.format(self.port))
+        log.debug('{0} | create socket'.format(self.port))
 
         self.sock.bind((self.ip, self.port))
-        log.debug('{} | bind to socket'.format(self.port))
+        log.debug('{0} | bind to socket'.format(self.port))
 
         self.sock.listen(5)
-        log.debug('{} | listen socket'.format(self.port))
+        log.debug('{0} | listen socket'.format(self.port))
 
-    @with_goto
     def create(self):
-        label.begin
-        self.conn, self.addr = self.sock.accept()
-        log.debug('{} | accept connection'.format(self.port))
-        self.conn.setblocking(1)
-
         while True:
-            try:
-                self.query_from_MES = self.conn.recv(10000).strip().decode(
-                    'utf-8')
+            self.conn, self.addr = self.sock.accept()
+            log.debug('{0} | accept connection'.format(self.port))
+            self.conn.setblocking(1)
 
-                if not len(self.query_from_MES) == 0:
-                    log.debug('{} | receive data    | {}'.format(self.port,
-                                                                 self.query_from_MES))
-                    flag = self.query_from_MES[0:3]
-                    log.debug(
-                        '{} | flag            | {}'.format(self.port, flag))
+            while True:
+                try:
+                    self.query_from_MES = self.conn.recv(10000).strip().decode('utf-8')
 
-                    if flag == 'REQ':
-                        answer = self.query_from_MES + work_with_DB.get_last_receiving_data(
-                            self.port)
-                        log.debug('{} | sent to MES     | {}'.format(self.port,
-                                                                     answer))
-                        self.conn.send(bytearray(answer, 'utf-8'))
-                    else:
-                        answer = work_with_DB.upload_data(
-                            self.query_from_MES[0:4000], self.port)
-                        return_msg = self.query_from_MES[0:45] + answer
-                        log.debug('{} | sent to MES     | {}'.format(self.port,
-                                                                     return_msg))
-                        self.conn.send(bytearray(return_msg, 'utf-8'))
+                    if not len(self.query_from_MES) == 0:
+                        log.debug('{0} | receive data    | {1}'.format(self.port,
+                                                                     self.query_from_MES))
+                        flag = self.query_from_MES[0:3]
+                        log.debug(
+                            '{0} | flag            | {1}'.format(self.port, flag))
 
-                        if answer[0:2] == conf.SEND_ERROR:
-                            log.warning(
-                                '{} | connection closed'.format(self.port))
-                            self.conn.close()
-                            try:
+                        if flag == 'REQ':
+                            answer = self.query_from_MES + work_with_DB.get_last_receiving_data(
+                                self.port)
+                            log.debug('{0} | sent to MES     | {1}'.format(self.port,
+                                                                         answer))
+                            self.conn.send(bytearray(answer, 'utf-8'))
+                        else:
+                            answer = work_with_DB.upload_data(
+                                self.query_from_MES[0:4000], self.port)
+                            return_msg = self.query_from_MES[0:45] + answer
+                            log.debug('{0} | sent to MES     | {1}'.format(self.port,
+                                                                         return_msg))
+                            self.conn.send(bytearray(return_msg, 'utf-8'))
+
+                            if answer[0:2] == conf.SEND_ERROR:
                                 log.warning(
-                                    '{} | restart server'.format(self.port))
-                                s1 = socket_server(self.ip, self.port)
-                                s1.create()
-                            except Exception as e:
-                                # returns the name of the exception
-                                log.exception('{} | {} | {}'.format(self.port,
-                                                                    type(
-                                                                        e).__name__,
-                                                                    e.message))
-                else:
-                    goto.begin
-                    log.debug('{} | goto .begin'.format(self.port))
-            except socket.error as e:
-                if e.errno == 10054:
-                    goto.begin
-                    log.exception(
-                        '{} | {} | {}'.format(self.port, type(e).__name__,
-                                              e.message))
+                                    '{0} | connection closed'.format(self.port))
+                                self.conn.close()
+                                break
+                    else:
+                        log.debug('{0} | port listening'.format(self.port))
+                        break
+                except socket.error as e:
+                    if e.errno == 10054:
+                        log.exception(
+                            '{0} | {1}'.format(self.port, type(e).__name__))
+                        break
+                except KeyboardInterrupt:
+                    log.exception('Process Interrupted')
